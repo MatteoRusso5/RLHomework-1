99cc3d2c43577ac03816eae7a7168e92e8c8a045
MatteoRusso5 <matteo.russo5@studenti.unina.it>
2025-01-04 19:51:59 +0100

    Update arm_control.launch.py

--- arm_control/launch/arm_control.launch.py
+++ arm_control/launch/arm_control.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         arguments=["position_controller", "--controller-manager", "/controller_manager"],  
         
     ) 
-    # --controller-manager is used to specify the name of the controller manager
+    # --controller-manager is used to specify the name of the controller manager which is defined in the yaml file
 
 
     nodes_to_start = [
